d56fef72ddfe58258d1b506c15f49cc5c6e18e60
omar2627 <omarfaruk2627bd@gmail.com>
2023-09-16 23:41:35 +0600

    W15 day-2 updated

--- WEEK-15/DAY-1/B_XOR_Palindromes.cpp
+++ WEEK-15/DAY-1/B_XOR_Palindromes.cpp
@@ -1,1 +1,68 @@
+/*
+PBL -->
+*/
 
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;cin>>t;
+    while(t--)
+    {
+        int n;cin>>n;
+        string s;cin>>s;
+        int pairr=0,minSetBit=0;
+        int i=0,j=n-1;
+        while(i<j)
+        {
+            if(s[i]==s[j])
+            {
+                pairr +=2;
+            }
+            else
+            {
+                minSetBit++;
+            }
+            i++;
+            j--;
+        }
+
+        cout<<pairr<<" "<<minSetBit<<endl;
+        vector<int>ans;
+        for(int i=0; i<=n; i++)
+        {
+            if(i<minSetBit)
+            {
+              ans.push_back(0);
+            }
+            else
+            {
+                if(pairr+minSetBit>=i)
+                {
+                    if(n%2!=0 && i%2!=0)
+                    {
+                        ans.push_back(1);
+                    }
+                    else if(i%2==0)
+                    {
+                        ans.push_back(1);
+                    }
+                    else
+                    {
+                        ans.push_back(0);
+                    }
+                }
+                else
+                {
+                    ans.push_back(0);
+                }
+            }
+        }
+        for(int i=0; i<ans.size(); i++)
+        {
+            cout<<ans[i];
+        }
+        cout<<endl;
+    }
+}
